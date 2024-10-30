a2db7938b1c955819733d0adab3f33178cfe0892
Hyunho Lee <37637823+danniely@users.noreply.github.com>
2024-10-30 03:32:30 -0500

    Title: 정수 n개의 합 2, Time: 5066ms, Memory: 41MB, Status: Time Limit Exceed - CodeTree

--- 241030/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 241030/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -7,13 +7,13 @@ arr = list(map(int, input().strip().split(" ")))
 # prefix: -10 0 -4 0 1
 
 def max_sum_k_consecutive(arr, k):
-    # prefix_sum = [0] + reduce(lambda acc, x: acc + [acc[-1]+x], arr[1:], [arr[0]])
+    prefix_sum = [0] + reduce(lambda acc, x: acc + [acc[-1]+x], arr[1:], [arr[0]])
     # prefix_sum = [0]
     # for elem in arr:
     #     prefix_sum.append(prefix_sum[-1]+elem)
-    prefix_sum = [0] * (len(arr) + 1)
-    for i in range(1, len(arr) + 1):
-        prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
+    # prefix_sum = [0] * (len(arr) + 1)
+    # for i in range(1, len(arr) + 1):
+    #     prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
 
     max_sum = -float('inf')
     for i in range(k,len(prefix_sum)):
