708ac0cac268f3dfc9430ae13ff42aea624b6f45
Hyunho Lee <37637823+danniely@users.noreply.github.com>
2024-10-31 03:28:57 -0500

    Title: 부분 수열의 합이 k, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.py
+++ 241031/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.py
@@ -1,21 +1,25 @@
-n, k = map(int, input().split())
+n, k = list(map(int, input().strip().split(" ")))
+arr = list(map(int, input().strip().split(" ")))
 
-arr = [0] + list(map(int, input().split()))
+def sumOfSubarraysEqualK(arr, n, k):
+    prefix_sum = [0]*(n+1)
 
-prefix_sum = [0 for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
-
-ans = -100 * 100000 * 100000
-
-# prefix cannot have the same number (the minimum value of given numbers is 1)
-
-nums = set(prefix_sum)
-ans = 0 
-
-for num in nums:
-    if (num - k) in nums:
-        ans += 1
+    # compute prefix sum
+    for i in range(n):
+        prefix_sum[i+1] = prefix_sum[i] + arr[i]
     
-print(ans)
+    L, R = 1,1
+    ans = 0
+    while L <= R and R < n+1:
+        if prefix_sum[R] - prefix_sum[L-1] == k:
+            ans += 1
+            L += 1
+            # R += 1
+        elif prefix_sum[R] - prefix_sum[L-1] > k:
+            L += 1
+        else:
+            R += 1
+    
+    return ans
+
+print(sumOfSubarraysEqualK(arr, n, k))
